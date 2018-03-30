94c5aa624167c5c43e050b801bd7dc8c7cf46047
Marc Lavergne <mlavergn@gmail.com>
2018-03-29 23:37:17 -0700

    Add: Initial file dump

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# aws-templates
+# AWS Templates
+
 Bare bones AWS templates
+
+NOTE: This is still a work in progress and I'm hoping to automate some of the AWS boilerplate.
---
+++ lambda/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// DemoEvent input
+type DemoEvent struct {
+	ID int `json:"id"`
+}
+
+// DemoResult output
+type DemoResult struct {
+	Message string `json:"result"`
+}
+
+// Handler lamda entry point
+func Handler(event DemoEvent) (DemoResult, error) {
+	return DemoResult{Message: fmt.Sprintf("Received ID [%d]", event.ID)}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
